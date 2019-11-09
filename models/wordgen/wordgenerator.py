59836af4c7fbc81390b0ed31a69b2108c02a757d
Le Van Hanh <hanhlv2@vng.com.vn>
2019-11-09 19:03:58 +0700

    Add

--- models/wordgen/wordgenerator.py
+++ models/wordgen/wordgenerator.py
@@ -1,12 +1,21 @@
 from models.model import *
 from system.helpers import data as dman
+from system.data_structures.sentence import Sentence
 
 class WordGenerator(Model):
+
+    collection_name = "word"
+    fields = [
+        "origin", 
+        "sentences", 
+        "labels"
+    ]
 
     def __init__(self, name, window_size, in_list):
         super().__init__(name)
         self.window_size = window_size
         self.in_list = set(in_list)
+        self.check_in_list = len(self.in_list) > 0
         self.sentences = []
         self.real_sentences = []
         self.onelabel = dict()
@@ -21,11 +30,8 @@ class WordGenerator(Model):
     def run(self):
         data = dman.create_window_items(self.sentences, self.window_size)
         self.result = dict()
-        self.log.start(len(data))
-        self.log.set_interval(1)
         for word, data in data.items():
-            self.log.progress([word])
-            if word not in self.in_list:
+            if (self.check_in_list) and (word not in self.in_list):
                 print("%s is not in template words --> skip!" % word)
                 continue
             sentences, labels = dman.separate_part(data, 2)
@@ -34,73 +40,34 @@ class WordGenerator(Model):
                 self.onelabel[word] = labels[0]
                 continue
             new_data = {
+                "origin" : word, 
                 "sentences" : sentences,
                 "labels" : labels
             }
-            # Save file now
-            if self.check_active("online"):
-                self.save_word(word, new_data)
+            self.result[word] = new_data
+        return self.result, self.onelabel
 
-            if self.returned:
-                self.result[word] = new_data
-            else:
-                del new_data
-
-        if self.check_active("after"):
-            self.save_data()
-
-        if self.returned:
-            return self.result, self.onelabel
-
-    def run_test(self):
-        self.result = []
-        for sentence, rsentence in zip(self.sentences, self.real_sentences):
-            data1 = dman.create_window_items([sentence], self.window_size)
-            data2 = dman.create_window_items([rsentence], self.window_size)
-            self.log.start(len(data1))
-            self.log.set_interval(1)
-            current_result = dict()
-            for word in data1:
-                self.log.progress([word])
-                if word not in self.in_list:
-                    print("%s is not in template words --> skip!" % word)
-                    continue
-                sentences, _ = dman.separate_part(data1[word], 2)
-                _, labels = dman.separate_part(data2[word], 2)
-
-                new_data = {
-                    "sentences": sentences,
-                    "labels": labels
-                }
-                current_result[word] = new_data
-            self.result.append(current_result)
-        return self.result
-
-    def save_data(self):
+    def save_words(self, db_instance):
+        data = []
         for word, cur_data in self.result.items():
-            self.save_word(word, cur_data)
+            item = dict()
+            for field in self.fields:
+                item[field] = cur_data[field]
+            data.append(item)
+        
+        for word, cur_data in self.onelabel.items():
+            data.append({
+                "origin" : word, 
+                "label" : cur_data
+            })
 
-    def save_word(self, word, data):
-        cur_dir = fman.join_path(self.filedir, word)
-        fman.make_dir(cur_dir)
-        for key, value in data.items():
-            filepath = fman.join_path(cur_dir, key + ".txt")
-            fman.save_text(value, filepath)
+        db_instance.write(self.collection_name, data)
 
-    def load_data(self, dir):
-        cnt, res = 0, dict()
-        name_loads = ["sentences", "labels"]
-        for word in self.in_list:
-            current_dir = fman.join_path(dir, word)
-            if not fman.check_folder(current_dir):
-                continue
-            data = dict()
-            for name in name_loads:
-                fullpath = fman.join_path(current_dir, name + ".txt")
-                if not fman.check_file(fullpath):
-                    break
-                data[name] = fman.load_text(fullpath)
-            if len(data) == len(name_loads):
-                cnt += 1
-                res[word] = data
-        return res
+    def get_words(self, db_instance, words):
+        data = dict()
+        for word in words:
+            db_data = db_instance.find(self.collection_name, {"origin" : word}, {"_id" : 0})
+            data[word] = dict()
+            for items in db_data:
+                data[word] = items
+        return data
--- test.py
+++ test.py
@@ -5,9 +5,15 @@ class Main:
     def __init__(self):
         self.loader = Loader()
         self.wordgen = WordGenerator("word-gen", 5, [])
-        db_data = self.loader.db.find("raw", {}, {"_id" : 0, "text" : 1})
-        data = []
-        for item in db_data:
-            data.append(item["text"])
+        # db_data = self.loader.db.find("raw", {}, {"_id" : 0, "text" : 1})
+        # data = []
+        # for item in db_data:
+        #     data.append(item["text"])
+        # print(data)
+
+        # self.wordgen.set_data(data)
+        # result, onelist = self.wordgen.run()
+        # self.wordgen.save_words(self.loader.db)
+        data = self.wordgen.get_words(self.loader.db, ["nhan", "dung"])
         print(data)
 Main()
