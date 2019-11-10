a85a47f1671592066358f9db4b0418ce5650ded3
Le Van Hanh <hanhlv2@vng.com.vn>
2019-11-10 13:36:33 +0700

    add

---
+++ models/wordgen/wordgenerator.py
@@ -0,0 +1,73 @@
+from models.model import *
+from system.helpers import data as dman
+from system.data_structures.sentence import Sentence
+
+class WordGenerator(Model):
+
+    collection_name = "word"
+    fields = [
+        "origin", 
+        "sentences", 
+        "labels"
+    ]
+
+    def __init__(self, name, window_size, in_list):
+        super().__init__(name)
+        self.window_size = window_size
+        self.in_list = set(in_list)
+        self.check_in_list = len(self.in_list) > 0
+        self.sentences = []
+        self.real_sentences = []
+        self.onelabel = dict()
+        self.sentence_instance = Sentence()
+        self.result = dict()
+
+    def set_data(self, sentences, real_sentences=[]):
+        """Data are only list of sentences"""
+        self.sentences = sentences
+        self.real_sentences = real_sentences
+
+    def run(self):
+        data = dman.create_window_items(self.sentences, self.window_size)
+        self.result = dict()
+        for word, data in data.items():
+            if (self.check_in_list) and (word not in self.in_list):
+                print("%s is not in template words --> skip!" % word)
+                continue
+            sentences, labels = dman.separate_part(data, 2)
+            # Word with only one fix
+            if len(set(labels)) == 1:
+                self.onelabel[word] = labels[0]
+                continue
+            new_data = {
+                "origin" : word, 
+                "sentences" : sentences,
+                "labels" : labels
+            }
+            self.result[word] = new_data
+        return self.result, self.onelabel
+
+    def save_words(self, db_instance):
+        data = []
+        for word, cur_data in self.result.items():
+            item = dict()
+            for field in self.fields:
+                item[field] = cur_data[field]
+            data.append(item)
+        
+        for word, cur_data in self.onelabel.items():
+            data.append({
+                "origin" : word, 
+                "label" : cur_data
+            })
+
+        db_instance.write(self.collection_name, data)
+
+    def get_words(self, db_instance, words):
+        data = dict()
+        for word in words:
+            db_data = db_instance.find(self.collection_name, {"origin" : word}, {"_id" : 0})
+            data[word] = dict()
+            for items in db_data:
+                data[word] = items
+        return data
